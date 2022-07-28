cfb75600e111d3b7f095182465c712f3b61df64f
김종원 <110171239+jongwoning1221@users.noreply.github.com>
2022-07-28 23:38:56 +0900

    0728

---
+++ 0728/Page122_4.java
@@ -0,0 +1,10 @@
+public class Page122_4 {
+    public static void main(String[]args) {
+        String a = "";
+
+        for(int i = 0; i < 5; i++) {
+            a += "*";
+            System.out.println(a);
+        }
+    }
+}
---
+++ 0728/Page123_5.java
@@ -0,0 +1,7 @@
+public class Page123_5 {
+    public static void main(String[]args) {
+        int a, b, c;
+
+        
+    }
+}
